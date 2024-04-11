51c5b074fd5a39f58de66b0140fd7270c7bdec21
amirsamtio@gmail.com <amirsamtio@gmail.com>
2024-04-12 03:37:01 +0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://veins-group-of-companies.web.app/',     
+    ],
+    methods: ["GET", "POST","PUT"],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
